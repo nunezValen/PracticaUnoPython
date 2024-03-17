bf39eb4baea6fc5956cbcbf64ebb97bfc16b6708
Valentin Nuñez <valenunez133@gmail.com>
2024-03-17 18:22:51 -0300

    Modificación del juego para que cuente la cantidad de fallos en lugar de tener un numero de intentos

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+max_attempts = 0
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 
@@ -17,16 +17,18 @@ word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
-for i in range(max_attempts):
+while max_attempts<10 :
      # Pedir al jugador que ingrese una letra
      letter = input("Ingresa una letra: ").lower()
      #Verifica si es una letra
      if not letter.isalpha():
          print("Por favor, ingresa solo letras.")
+         max_attempts=max_attempts+1
          continue          
      # Verificar si la letra ya ha sido adivinada
      if letter in guessed_letters:
          print("Ya has intentado con esa letra. Intenta con otra.")
+         max_attempts=max_attempts+1
          continue
      # Agregar la letra a la lista de letras adivinadas
      guessed_letters.append(letter)
@@ -35,6 +37,7 @@ for i in range(max_attempts):
          print("¡Bien hecho! La letra está en la palabra.")
      else:
          print("Lo siento, la letra no está en la palabra.")
+         max_attempts=max_attempts+1
      # Mostrar la palabra parcialmente adivinada
      letters = []
      for letter in secret_word:
@@ -46,8 +49,8 @@ for i in range(max_attempts):
      print(f"Palabra: {word_displayed}")
      # Verificar si se ha adivinado la palabra completa
      if word_displayed == secret_word:
-         print(f"¡Felicidades! Has adivinado la palabra secreta:  {secret_word}")
+         print(f"¡Felicidades! Has adivinado la palabra secreta:  {secret_word} y con tan solo {max_attempts} fallos")
          break
 else:
-     print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+     print(f"¡Oh no! Has llegado a {max_attempts} fallos.")
      print(f"La palabra secreta era: {secret_word}")
